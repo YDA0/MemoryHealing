e44be58b66b19c97c7a117eef60b94b48f977960
YDA0 <dayoung9746@gmail.com>
2023-05-29 22:50:33 +0900

    firebase 연동 및 로그인/회원가입

---
+++ chat/chatting/ChatViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ChatViewController.swift
+//  chatting
+//
+//  Created by 장세림 on 2023/05/26.
+//
+
+import Foundation
+import FirebaseAuth
+
+class ChatViewController: ViewController {
+    
+//    @IBAction func logout(_ sender: UIBarButtonItem) {
+//        let firebaseAuth = Auth.auth ()
+//        do {
+//            try firebaseAuth.signOut ( )
+//            self.navigationCont roller? .popToRootViewController(animated: true)
+//        } catch let signOutError as NSError {
+//            print ("Error signing out: %@", signOutError)
+//        }
+//
+//    }
+}
